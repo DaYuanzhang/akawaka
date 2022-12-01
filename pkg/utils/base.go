58928a2620d193d580976122c3352879d645bdd9
dean <dean@dean.com>
2022-12-01 15:34:11 +0800

    version 0.0.3
    fix 2 bug

--- pkg/utils/base.go
+++ pkg/utils/base.go
@@ -6,18 +6,14 @@ import (
 	"strings"
 )
 
-func Transform(options *config.Options) error {
+func KeywordTransform(options *config.Options) error {
+	// 判断关键词输入方式是文本还是多个字符串
 	if len(options.Keyword) != 0 {
-		if strings.Contains(options.Keyword, ",") {
+		if strings.Contains(options.Keyword, ",") { //输入形式为：-k "a,b,c"
 			options.SetKeywords()
-		} else {
-			options.Keywords = append(options.Keywords, options.Keyword)
-		}
-		if strings.Contains(options.Extens, ",") {
-			options.SetExtensions()
 			return nil
-		} else {
-			options.Extensions = append(options.Extensions, options.Extens)
+		} else { // 输入形式为 -k "a"
+			options.Keywords = append(options.Keywords, options.Keyword)
 			return nil
 		}
 	} else if len(options.Keywords_File) != 0 {
@@ -26,15 +22,39 @@ func Transform(options *config.Options) error {
 		if err != nil {
 			return err
 		}
+		return nil
+	} else {
+		return errors.New("keyword is required...")
+	}
+}
+
+func ExtensTransform(options *config.Options) error {
+	if len(options.Extens) != 0 {
 		if strings.Contains(options.Extens, ",") {
 			options.SetExtensions()
 			return nil
 		} else {
-			options.Extensions = append(options.Extensions, strings.TrimSpace(options.Extens))
+			options.Extensions = append(options.Extensions, options.Extens)
 			return nil
 		}
 	} else {
-		return errors.New("keyword is required...")
+		return errors.New("extension is required...")
+	}
+}
+
+func Transform(options *config.Options) error {
+
+	// 转换关键字属性
+	err1 := KeywordTransform(options)
+	if err1 != nil {
+		return err1
 	}
 
+	// 转换后缀名属性
+	err2 := ExtensTransform(options)
+	if err2 != nil {
+		return err2
+	}
+
+	return nil
 }
